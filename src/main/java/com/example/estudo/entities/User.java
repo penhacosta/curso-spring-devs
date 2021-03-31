6c86a22f37b644d2bc838a894374e3d43631660e
Adevaldo Penha Costa <adevaldo-costa@hotmail.com>
2021-03-30 21:10:52 -0300

    Conexao com o banco de dados

--- src/main/java/com/example/estudo/entities/User.java
+++ src/main/java/com/example/estudo/entities/User.java
@@ -1,8 +1,16 @@
 package com.example.estudo.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements java.io.Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String email;
